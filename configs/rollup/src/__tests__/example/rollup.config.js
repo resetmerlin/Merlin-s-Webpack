5fb910db71b5c924d3b3528cae5893d389fa0e7c
resetmerlin <resetmerlin@gmail.com>
2024-12-22 22:30:59 +0900

    feat: Add test code of the rollup plugin

---
+++ configs/rollup/src/__tests__/example/rollup.config.js
@@ -0,0 +1,5 @@
+import { enchantDefaultRollupConfig } from '@enchant.js/rollup-config';
+
+module.exports = enchantDefaultRollupConfig({
+  packageDir: __dirname,
+});
---
+++ configs/rollup/src/__tests__/handle-entry.test.js
@@ -0,0 +1,24 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+const { handleCJSEntrypoint, handleESMEntrypoint } = require('../handle-entry');
+const pkg = require('./example/package.json');
+
+describe('It should retrieve entry point', () => {
+  const entrypoints = Object.keys(pkg.exports).filter(key => key !== './package.json');
+
+  it('get cjs entry point', () => {
+    entrypoints.forEach(entryPoint => {
+      if (pkg.exports[entryPoint]) {
+        expect(handleCJSEntrypoint(pkg.exports, entryPoint)).toBe(pkg.exports[entryPoint].require);
+      }
+    });
+  });
+
+  it('get esm entry point', () => {
+    entrypoints.forEach(entryPoint => {
+      if (pkg.exports[entryPoint]) {
+        expect(handleESMEntrypoint(pkg.exports, entryPoint)).toBe(pkg.exports[entryPoint].import);
+      }
+    });
+  });
+});
