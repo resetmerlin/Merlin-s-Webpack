10381a37d999644b8aaa1fe2bed5ae1e1b509020
resetmerlin <resetmerlin@gmail.com>
2024-12-30 15:11:24 +0900

    refactor: Refactor the code

--- scripts/node-deps-check.js
+++ scripts/node-deps-check.js
@@ -1,202 +1,234 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const glob = require('glob');
-const path = require('path');
-const packageJson = require('../package.json');
-const fs = require('fs');
 const chalk = require('chalk');
 const inquirer = require('inquirer');
+const { ROOT, INQUIRER_PROMPT } = require('./constants');
+const { readAndParseFile, writeOnFile } = require('./utils');
 
-const workspaces = packageJson.workspaces;
+/**
+ * Get all `package.json` file paths from the specified workspaces.
+ * @returns {string[]} Array of paths to `package.json` files.
+ */
+function getAllPackageJsonDir() {
+  const workspaces = ROOT.PACKAGE_JSON_CONTENT.workspaces;
 
-const monorepoRoot = path.resolve(process.cwd());
+  if (workspaces == null) {
+    console.log(chalk.red('You need to specify your workspaces'));
+    return [];
+  }
 
-const packagePathLists = workspaces.flatMap(workspace =>
-  glob.sync(`${workspace}/package.json`, {
-    cwd: monorepoRoot,
-    ignore: ['**/node_modules/**', '**/.git/**', '**/.yarn/**'],
-    absolute: true,
-  })
-);
-
-const dependencyPerPackageTable = new Map();
-
-for (const packageJsonDir of packagePathLists) {
-  const file = JSON.parse(fs.readFileSync(packageJsonDir, 'utf8'));
-
-  const devDependencies = file.devDependencies ?? {};
-  const dependencies = file.dependencies ?? {};
-
-  dependencyPerPackageTable.set(packageJsonDir, { ...devDependencies, ...dependencies });
+  return workspaces.flatMap(workspace => {
+    return glob.sync(`${workspace}/package.json`, {
+      cwd: ROOT.PATH,
+      ignore: ['**/node_modules/**', '**/.git/**', '**/.yarn/**'],
+      absolute: true,
+    });
+  });
 }
 
-const totalPackageVersionTable = new Map();
+/**
+ * Generate a dependency graph for each `package.json` file.
+ * @param {string[]} pkgPathLists - Array of paths to `package.json` files.
+ * @returns {Map<string, object>} A map of `package.json` paths to their dependencies.
+ */
+function getDepsGraphPerPackage(pkgPathLists) {
+  const depsPerPkg = new Map();
 
-for (const dependencies of dependencyPerPackageTable.values()) {
-  for (const [name, version] of Object.entries(dependencies)) {
-    if (!totalPackageVersionTable.has(name)) {
-      totalPackageVersionTable.set(name, new Map());
+  for (const pkgJsonPath of pkgPathLists) {
+    const file = readAndParseFile(pkgJsonPath);
+
+    const devDependencies = file?.devDependencies ?? {};
+    const dependencies = file?.dependencies ?? {};
+
+    depsPerPkg.set(pkgJsonPath, { ...devDependencies, ...dependencies });
+  }
+
+  return depsPerPkg;
+}
+
+/**
+ * Generate a map of all dependencies with their respective versions and occurrences.
+ * @param {Map<string, object>} depsPerPkg - Dependency graph for each `package.json` file.
+ * @returns {Map<string, Map<string, number>>} A map of dependency names to a map of versions and their occurrences.
+ */
+function getEntireDepsWithVersions(depsPerPkg) {
+  const entirePkgVersions = new Map();
+
+  for (const dependencies of depsPerPkg.values()) {
+    for (const [name, version] of Object.entries(dependencies)) {
+      if (!entirePkgVersions.has(name)) {
+        entirePkgVersions.set(name, new Map());
+      }
+
+      const versionVariety = entirePkgVersions.get(name);
+      versionVariety.set(version, versionVariety.get(version) + 1 || 1);
+    }
+  }
+
+  for (const name of entirePkgVersions.keys()) {
+    let totalOccurrences = 0;
+
+    const versions = entirePkgVersions.get(name);
+
+    for (const frequency of versions.values()) {
+      totalOccurrences += frequency;
     }
 
-    const versionVarietyMap = totalPackageVersionTable.get(name);
-    versionVarietyMap.set(version, versionVarietyMap.get(version) + 1 || 1);
+    if (totalOccurrences === 1) {
+      entirePkgVersions.delete(name);
+    }
+  }
+
+  return entirePkgVersions;
+}
+
+/**
+ * Rewrite `package.json` files to resolve version mismatches.
+ * @param {object} params - Parameters for rewriting files.
+ * @param {string} [params.version] - Selected version to use.
+ * @param {string} [params.depsType] - Dependency type (`dependencies` or `devDependencies`).
+ * @param {Map<string, object>} params.depsGraphPerPkg - Dependency graph for each `package.json` file.
+ * @param {string} params.pkgName - Name of the package to rewrite.
+ */
+function rewritePkgJsonFile({ version, depsType, depsGraphPerPkg, pkgName }) {
+  if (version == null && depsType == null) {
+    rewriteChildPkgJsonFile(depsGraphPerPkg, pkgName);
+    return;
+  }
+
+  rewriteRootPkgJsonFile(depsType, version);
+  rewriteChildPkgJsonFile(depsGraphPerPkg, pkgName);
+}
+
+/**
+ * Rewrite the root `package.json` file with the specified dependency and version.
+ * @param {string} depsType - Dependency type (`dependencies` or `devDependencies`).
+ * @param {string} version - Selected version to use.
+ */
+function rewriteRootPkgJsonFile(depsType, version) {
+  const file = { ...ROOT.PACKAGE_JSON_CONTENT };
+  file[depsType][pkgName] = version;
+  writeOnFile(ROOT.PACKAGE_JSON, file);
+}
+
+/**
+ * Rewrite child `package.json` files to use workspace references (`workspace:*`) for a given package.
+ * @param {Map<string, object>} depsGraphPerPkg - Dependency graph for each `package.json` file.
+ * @param {string} pkgName - Name of the package to rewrite.
+ */
+function rewriteChildPkgJsonFile(depsGraphPerPkg, pkgName) {
+  const dirLists = [];
+
+  for (const dir of depsGraphPerPkg.keys()) {
+    if (dir === ROOT.PACKAGE_JSON) continue;
+    const deps = depsGraphPerPkg.get(dir);
+
+    if (deps[pkgName]) {
+      dirLists.push(dir);
+    }
+  }
+
+  for (const dir of dirLists) {
+    const file = readAndParseFile(dir);
+
+    if (file.dependencies?.[pkgName]) {
+      file.dependencies[pkgName] = 'workspace:*';
+    } else if (file.devDependencies?.[pkgName]) {
+      file.devDependencies[pkgName] = 'workspace:*';
+    }
+
+    writeOnFile(dir, file);
   }
 }
 
-for (const name of totalPackageVersionTable.keys()) {
-  let totalOccurrences = 0;
+/**
+ * Fix version mismatches across the monorepo by rewriting `package.json` files.
+ * @param {Map<string, object>} depsGraphPerPkg - Dependency graph for each `package.json` file.
+ */
+async function fixVersionMismatches(depsGraphPerPkg) {
+  const entirePkgVersions = getEntireDepsWithVersions(depsGraphPerPkg);
 
-  const versionVarietyMap = totalPackageVersionTable.get(name);
+  const rootPkgDeps = new Map(
+    Object.entries({
+      ...(ROOT.PACKAGE_JSON_CONTENT?.devDependencies ?? {}),
+      ...(ROOT.PACKAGE_JSON_CONTENT?.dependencies ?? {}),
+    })
+  );
 
-  for (const frequency of versionVarietyMap.values()) {
-    totalOccurrences += frequency;
-  }
+  for (const pkgName of entirePkgVersions.keys()) {
+    const pkgVersions = entirePkgVersions.get(pkgName);
 
-  if (totalOccurrences === 1) {
-    totalPackageVersionTable.delete(name);
-  }
-}
-
-const rootPackageTable = new Map(Object.entries({ ...packageJson.devDependencies, ...packageJson.dependencies }));
-
-async function fixVersionMismatches() {
-  const depTypeOptions = [
-    { name: 'dependency', value: 'dependencies' },
-    { name: 'devDependency', value: 'devDependencies' },
-  ];
-
-  for (const pkgName of totalPackageVersionTable.keys()) {
-    const versionVarietyMap = totalPackageVersionTable.get(pkgName);
-
-    if (rootPackageTable.has(pkgName)) {
-      if (versionVarietyMap.size === 1 && versionVarietyMap.has('workspace:*')) continue;
+    if (rootPkgDeps.has(pkgName)) {
+      if (pkgVersions.size === 1 && pkgVersions.has('workspace:*')) continue;
 
       console.log(
         chalk.red(`The package `) +
           chalk.bold.underline.red(`${pkgName}`) +
-          chalk.red(
-            ` is listed in the root. However, the version used in child packages does not match the root version.`
-          )
+          chalk.red(` is listed in the root, but the version used in child packages does not match the root version.`)
       );
 
-      const directoryLists = retrieveDir(pkgName);
-
-      for (const dir of directoryLists) {
-        updatePackageJson(dir, pkgName, 'workspace:*');
-      }
+      rewritePkgJsonFile({ depsGraphPerPkg, pkgName });
     } else {
-      if (versionVarietyMap.size === 1) {
-        const keys = [...versionVarietyMap.keys()];
-        for (const key of keys) {
-          console.log(
-            chalk.red(`The package `) +
-              chalk.bold.underline.red(`${pkgName}`) +
-              chalk.red(
-                ` is not listed in the root. However, it is used in child packages with the following version:`
-              ) +
-              chalk.bold.underline.greenBright(` ${key}`)
-          );
-        }
-
-        const directoryLists = retrieveDir(pkgName);
-
-        const rootPkg = JSON.parse(fs.readFileSync(path.join(monorepoRoot, 'package.json'), 'utf8'));
-
-        const res = await inquirer.prompt([
-          {
-            type: 'list',
-            name: 'selection',
-            message: `The dependency ${pkgName} is used in child packages but not listed in the root package.json.\nChoose the type of dependency to add it as:`,
-            choices: depTypeOptions,
-          },
-        ]);
-
-        rootPkg[res.selection] = rootPkg[res.selection] || {};
-        rootPkg[res.selection][pkgName] = keys[0];
-        fs.writeFileSync(path.join(monorepoRoot, 'package.json'), JSON.stringify(rootPkg, null, 2), 'utf8');
-
-        for (const dir of directoryLists) {
-          updatePackageJson(dir, pkgName, 'workspace:*');
-        }
-      } else {
-        const keys = [...versionVarietyMap.keys()];
+      if (pkgVersions.size === 1) {
+        const version = [...pkgVersions.keys()].shift();
 
         console.log(
           chalk.red(`The package `) +
             chalk.bold.underline.red(`${pkgName}`) +
-            chalk.red(` is not listed in the root. However, it is used in child packages with multiple versions:`) +
-            chalk.bold.underline.greenBright(` ${keys.join(', ')}`)
+            chalk.red(` is not listed in the root, but it is used in child packages with the following version:`) +
+            chalk.bold.underline.greenBright(` ${version}`)
         );
 
-        const directoryLists = retrieveDir(pkgName);
-
-        const rootPkg = JSON.parse(fs.readFileSync(path.join(monorepoRoot, 'package.json'), 'utf8'));
-
-        const depVersionOptions = keys.map(val => {
-          return {
-            name: val,
-            value: val,
-          };
-        });
-
-        const resVersion = await inquirer.prompt([
+        const depsType = await inquirer.prompt([
           {
-            type: 'list',
-            name: 'selection',
+            ...INQUIRER_PROMPT.DEFAULT_CONFIG,
+            message: `The dependency ${pkgName} is used in child packages but is not listed in the root package.json.\nChoose the type of dependency to add it as:`,
+          },
+        ]);
+
+        rewritePkgJsonFile({ version, depsType: depsType.selection, depsGraphPerPkg, pkgName });
+      } else {
+        const versions = [...pkgVersions.keys()];
+
+        console.log(
+          chalk.red(`The package `) +
+            chalk.bold.underline.red(`${pkgName}`) +
+            chalk.red(` is not listed in the root, but it is used in child packages with multiple versions:`) +
+            chalk.bold.underline.greenBright(` ${versions.join(', ')}`)
+        );
+
+        const version = await inquirer.prompt([
+          {
+            ...INQUIRER_PROMPT.DEFAULT_CONFIG,
             message: `The dependency ${pkgName} is used in child packages with multiple versions. Select the appropriate version to use:`,
-            choices: depVersionOptions,
+            choices: versions.map(val => {
+              return {
+                name: val,
+                value: val,
+              };
+            }),
           },
         ]);
 
-        const res = await inquirer.prompt([
+        const depsType = await inquirer.prompt([
           {
-            type: 'list',
-            name: 'selection',
-            message: `You selected version ${resVersion.selection} for ${pkgName}, but it is not listed in the root package.json.\nChoose the type of dependency to add it as:`,
-            choices: depTypeOptions,
+            ...INQUIRER_PROMPT.DEFAULT_CONFIG,
+            message: `You selected version ${version.selection} for ${pkgName}, but it is not listed in the root package.json.\nChoose the type of dependency to add it as:`,
           },
         ]);
 
-        rootPkg[res.selection] = rootPkg[res.selection] || {};
-        rootPkg[res.selection][pkgName] = resVersion.selection;
-        fs.writeFileSync(path.join(monorepoRoot, 'package.json'), JSON.stringify(rootPkg, null, 2), 'utf8');
-
-        for (const dir of directoryLists) {
-          updatePackageJson(dir, pkgName, 'workspace:*');
-        }
+        rewritePkgJsonFile({ version: version.selection, depsType: depsType.selection, depsGraphPerPkg, pkgName });
       }
     }
   }
 }
 
-function retrieveDir(name) {
-  const stack = [];
-  for (const key of dependencyPerPackageTable.keys()) {
-    const map = dependencyPerPackageTable.get(key);
-
-    if (map[name]) {
-      stack.push(key);
-    }
-  }
-
-  return stack.filter(val => val !== path.join(monorepoRoot, 'package.json'));
-}
-
-function updatePackageJson(dir, pkgName, version) {
-  const packagePath = path.resolve(dir);
-  const packageData = JSON.parse(fs.readFileSync(packagePath, 'utf-8'));
-
-  if (packageData.dependencies?.[pkgName]) {
-    packageData.dependencies[pkgName] = version;
-  } else if (packageData.devDependencies?.[pkgName]) {
-    packageData.devDependencies[pkgName] = version;
-  }
-
-  fs.writeFileSync(packagePath, JSON.stringify(packageData, null, 2), 'utf8');
-}
+const pkgJsonDirLists = getAllPackageJsonDir();
+const depsGraphPerPkg = getDepsGraphPerPackage(pkgJsonDirLists);
 
 (async () => {
   try {
-    await fixVersionMismatches();
+    await fixVersionMismatches(depsGraphPerPkg);
     console.log(chalk.green('All mismatches have been resolved!'));
   } catch (err) {
     console.error(chalk.red('An error occurred:'), err);
